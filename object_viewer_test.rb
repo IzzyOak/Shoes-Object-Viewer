c929e4b9d09737b3904968bd7ffc82aa688f7e19
Isobel Oakley <TeamNexium@users.noreply.github.com>
2018-01-29 18:54:35 +0000

    Add files via upload

---
+++ object_viewer_test.rb
@@ -0,0 +1,27 @@
+Shoes.app :title => "Shoes Object Viewer" do
+
+  stack :margin => 20 do
+    flow do
+      para "Object: "
+      edit_line do |input_box|
+        begin
+          selected_class = Kernel.const_get(input_box.text)
+
+          @output.text = "Class method: 
+#{"=" * 10} 
+
+#{selected_class.methods.sort}"
+        
+
+
+
+        rescue
+          @output.text = "There isn't a Ruby class called #{input_box.text}"
+        end
+      end
+    end
+
+    @output = edit_box width: "100%", height: 400
+  end
+
+end
